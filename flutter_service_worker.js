bd4f670aca1ced87c63d8e863ff4db3e23e2f07b
Nrup Parikh <108717119+NrupParikh@users.noreply.github.com>
2025-02-12 11:33:39 +0530

    Add files via upload
    commit2

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -52,8 +52,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "f385b6d01aa72ca4c269966b52d8884f
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "86b414ab505bf95aba46c19dc4b8560a",
-"/": "86b414ab505bf95aba46c19dc4b8560a",
+"index.html": "72b88747fb3e2a5af70c82b9cf8c7ee9",
+"/": "72b88747fb3e2a5af70c82b9cf8c7ee9",
 "main.dart.js": "d6991da516d009de5789c5a5d74b6fad",
 "manifest.json": "bf24c84c3bf99672a631c4f84464e793",
 "version.json": "15235b5108d6a877ef74fe3317a96bf7"};
